48d3352688f0c126dd8ef2223d914916d80dda74
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2019-10-06 15:10:08 +0600

    Add files via upload

---
+++ hackerrank1.java
@@ -0,0 +1,43 @@
+
+package prec;
+
+import java.util.Scanner;
+
+public class hackerrank1 {
+     public static void main(String[] args) {
+         
+         Scanner sc =new Scanner(System.in);
+         
+           System.out.println("================================");
+          
+           
+           for( int i=0;i<3;i++){
+           String s = sc.next();
+           int x = sc.nextInt();
+            
+             System.out.printf("%-15s%03d%n", s, x);
+           
+            
+           }
+           
+           System.out.println("================================");
+           
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
