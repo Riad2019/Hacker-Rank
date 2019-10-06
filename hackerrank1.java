08f4251fc7dc921e7644aeb42e862f00b0f4a3d5
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2019-10-06 19:15:22 +0600

    Add files via upload

---
+++ hackerrank1.java
@@ -0,0 +1,53 @@
+
+package prec;
+
+import java.util.Scanner;
+
+public class hackerrank1 {
+     public static void main(String[] args) {
+       int result;
+         Scanner sc =new Scanner(System.in);
+         
+           int N = sc.nextInt();
+          
+           
+           for( int i=1;i<=10;i++){
+                result=N*i;
+               System.out.println(N+" x "+i+" = "+result);
+               //System.out.printf("%d * %d = %d%n",N,i,N*i);
+          
+            
+            
+           
+            
+           }
+           
+          
+           
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
