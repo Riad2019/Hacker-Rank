3bc71b2ec8bd0282e9787d6b38aa61ff3b150396
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2019-10-07 17:38:59 +0600

    Add files via upload

---
+++ DecimalFormatDemo.java
@@ -0,0 +1,43 @@
+
+package prec;
+
+
+import java.util.Scanner;
+
+
+
+public class DecimalFormatDemo {
+    public static void main(String[] args) {
+        
+        Scanner sc= new Scanner (System.in);
+        int x=sc.nextInt();
+        for(int i=0;i<x;i++){
+        
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int n= sc.nextInt();
+        for(int j=0;j<n;j++ ){
+            
+        a= a+((int)Math.pow(2,j)*b);    
+            System.out.print(a+" ");   
+            
+        } 
+            System.out.println("");
+        
+        }
+                 
+        
+    }
+       
+    
+    
+   }
+
+
+
+
+
+
+
+
+
