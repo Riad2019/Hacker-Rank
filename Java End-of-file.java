08efe01b406243c061fc9eb4af142696e0f35820
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2019-11-30 19:42:02 +0600

    Create Java End-of-file.java

---
+++ Java End-of-file.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+         Scanner in= new Scanner(System.in);
+        int i=1;
+        while(in.hasNext()){
+            System.out.println(i+" "+in.nextLine());
+            i++;
+        
+        }
+        
+        in.close();
+    }
+}
+
