a491ab955aefe068be377a7aee09e998e32f3ad8
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2020-02-12 05:30:29 +0800

    Update and rename Java Date and Time to Java Date and Time.java

--- Java Date and Time.java
+++ Java Date and Time.java
@@ -54,5 +54,25 @@ public class Solution {
 }
 
 
-
+/*       import java.time.*;
+*        import java.util.Scanner;
+*
+*
+*
+*   public class HCKR {
+*   public static void main(String[] args) {
+*
+*       Scanner in = new Scanner(System.in);
+*        int mm = in.nextInt();
+*       int dd = in.nextInt();
+*        int yy = in.nextInt();
+*
+*       LocalDate dt = LocalDate.of(yy,mm,dd);
+*        System.out.print(dt.getDayOfWeek());
+*
+*    }
+*
+*
+*   }
+*/
 
