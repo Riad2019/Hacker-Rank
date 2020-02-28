5e2c0b72d02da5688cae74e51204ee26cd08fe1b
Md. Abdur Rahman <46405817+Riad2019@users.noreply.github.com>
2020-02-29 01:30:29 +0800

    Create Same Text.java

---
+++ Same Text.java
@@ -0,0 +1,12 @@
+
+ import java.util.Scanner;  
+ //1124  
+ public class CelebrityJeopardy {  
+      public static void main(String[] args) {  
+           Scanner scanner = new Scanner(System.in);  
+           while(scanner.hasNext()) {  
+                System.out.println(scanner.nextLine());  
+           }  
+           scanner.close();  
+      }  
+ }  
